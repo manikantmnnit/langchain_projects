8cd6c9f10a797c89c1eacc544d9640944ba414eb
Mani K Kumar <manikantmnnit@email.com>
2025-03-01 10:32:50 -0600

    prompt: add chatbot.py using chat history list

--- langchain_prompt/prompt_basic/chatbot.py
+++ langchain_prompt/prompt_basic/chatbot.py
@@ -6,13 +6,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm=ChatOpenAI(
-    llm='gpt-4',
+    model_name="gpt-4",
     temperature=0.5)
 
-# create a infinite loop until user wants exit
+
+# # create a infinite loop until user wants exit
+# while True:
+#     user_input=input('you:  ')
+#     if user_input.lower() == 'exit':
+#         break
+#     result=llm.invoke(user_input)
+#     print('AI: {result.content}')
+
+# major problem is that chatbot never remember the previous chat
+# Initialize chat history
+chat_history = []
+
 while True:
-    user_input=input('you:  ')
-    if user_input=='Exit':
+    user_input = input('You: ')
+    if user_input.lower() == 'exit':  # Exit condition
         break
-    result=llm.invoke(user_input)
+
+    chat_history.append(user_input)  # Store user input
+    result = llm.invoke(chat_history)  # Invoke LLM model
+    ai_response = result.content
+    chat_history.append(ai_response)  # Store AI response
+    print(f'AI: {ai_response}')
+
+print(chat_history)
 
